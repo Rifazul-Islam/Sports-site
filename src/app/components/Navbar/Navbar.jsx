277acacfc2b20470ae7fcafa3d6b783b1c3edc0d
Rifazul <rifazul50@gmail.com>
2024-03-22 22:01:26 +0600

    next js first project set up

---
+++ src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="bg-blue-100 shadow-md py-4 flex justify-between items-center px-5">
+      <div className="text-2xl font-bold">Sports</div>
+      <div>Icons</div>
+    </div>
+  );
+};
+
+export default Navbar;
